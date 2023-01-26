420098fbd67718fd37f15a8b0d7061399a68626f
Joanna Patyk <patykjoanna93@gmail.com>
2023-01-27 00:30:37 +0100

    Project setup, backend server and REST API

---
+++ backend/actions/api/expenseActions.js
@@ -0,0 +1,48 @@
+const Expense = require('../../database/models/expenseModel');
+
+class ExpenseActions {
+    async save(req, res) {
+        const category = req.body.category;
+        const amount = req.body.amount;
+        const comment = req.body.comment;
+        let expense;
+
+        try {
+            expense = new Expense({ category, amount, comment });
+            await expense.save();
+        } catch (error) {
+            return res.status(422).json({ message: error.message });
+        }
+
+        res.status(201).json(expense);
+    }
+
+    async get(req, res) {
+        const expenses = await Expense.find({});
+
+        res.status(200).json(expenses);
+    }
+
+    async update(req, res) {
+        const category = req.body.category;
+        const amount = req.body.amount;
+        const comment = req.body.comment;
+        const id = req.params.id;
+
+        const expense = await Expense.findOne({ _id: id });
+        expense.category = category;
+        expense.amount = amount;
+        expense.comment = comment;
+        await expense.save();
+
+        res.status(201).json(expense);
+    }
+
+    async delete(req, res) {
+        const id = req.params.id;
+        await Expense.deleteOne({ _id: id });
+        res.sendStatus(204);
+    }
+}
+
+module.exports = new ExpenseActions();
