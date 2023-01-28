be52a6dc7b1a9da86fbae8b9f23b4be2ec8931dd
Joanna Patyk <patykjoanna93@gmail.com>
2023-01-28 19:54:01 +0100

    Pages

---
+++ frontend/src/assets/wrappers/SharedLayout.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+    .dashboard {
+        display: flex;
+        width: 90vw;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: 2rem;
+    }
+`;
+export default Wrapper;
