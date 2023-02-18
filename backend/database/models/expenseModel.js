a6dbdea62feb82dc1c803d34f1fcae6c07b6bf83
Joanna Patyk <patykjoanna93@gmail.com>
2023-02-18 20:50:32 +0100

    Add date to expenses -backend

--- backend/database/models/expenseModel.js
+++ backend/database/models/expenseModel.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 const ExpenseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
     category: {
         type: String,
         required: true
