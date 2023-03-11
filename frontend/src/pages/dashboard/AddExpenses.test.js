b1b1a5c83e270331c1dba41ac78924798b7bf351
Joanna Patyk <patykjoanna93@gmail.com>
2023-03-11 21:47:16 +0100

    wip - check if tests work

---
+++ frontend/src/pages/dashboard/AddExpenses.test.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import AddExpenses from './AddExpenses';
+
+describe('Calculator tests', () => {
+    test('adding 1 + 2 should return 3', () => {
+        expect(1 + 2).toBe(3);
+    });
+});
